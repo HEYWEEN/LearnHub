1911be5269bfcbbaf49db8f0a01b19930e8e361a
241276025-Chen Xufeng <241276025@smail.nju.edu.cn>
2025-11-23 15:15:10 +0800

    添加了防止bug的镇码之宝

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,31 @@
 import app from './app.js';
-import LOG_COLOR from './constants/logColor.js';
 
 const PORT = process.env.PORT || 3000;
+
+import LOG_COLOR from './constants/logColor.js';
+console.log(
+LOG_COLOR.BG_YELLOW+"///////////////////////////////////////////////////////////////"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                          _ooOoo_                          //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                         o8888888o                         //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                         88\" . \"88                         //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                         (| ^_^ |)                         //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                         O\\  =  /O                         //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                      ____/`---'\\____                      //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                    .'  \\\\|     |//  `.                    //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                   /  \\\\|||  :  |||//  \\                   //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                  /  _||||| -:- |||||-  \\                  //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                  |   | \\\\\\  -  /// |   |                  //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                  | \\_|  ''\\---/''  |   |                  //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                  \\  .-\\__  `-`  ___/-. /                  //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                ___`. .'  /--.--\\  `. . ___                //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//               .\"\" '<  `.___\\_<|>_/___.'  >'\"\".            //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//            | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |            //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//            \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /            //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//     ========`-.____`-.___\\_____/___.-`____.-'========     //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//                          `=---='                          //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^     //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"//           佛祖保佑       永不宕机     永无BUG             //"+LOG_COLOR.RESET+'\n'+
+LOG_COLOR.BG_YELLOW+"///////////////////////////////////////////////////////////////"+LOG_COLOR.RESET+'\n');
 
 app.listen(PORT, () => {
     console.log(LOG_COLOR.FG_BLUE + `LearnHub API is listening at http://localhost:${PORT}` + LOG_COLOR.RESET);
